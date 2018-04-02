5d9b963002d3663491b1a28684f00e6b3d6c0069
RichardKavanagh <richard.kavanagh7@mail.dcu.ie>
2018-04-02 22:18:59 +0100

    Added ycsb lab 5

--- labs/lab5/hbase_ycsb_benchtest.sh
+++ labs/lab5/hbase_ycsb_benchtest.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Commands used echo, exec sudo, cd
+# Commands used echo, exec sudo, cd, cat
 
 echo 'Logging in as hduser ...'
 HOME_NEW=/home/hduser
 exec sudo -u hduser /bin/sh - << eof
 cd $HOME_NEW
 
+echo 'Dropping old Hbase usertable table ...'
+cd hbase
+cat drop_test_data_hbase.txt | /home/hduser/hbase shell
 
 echo 'Creating a Hbase table called usertable with a single column family names cf1 ...'
-cd hbase
-./hbase shell ./hbase_table_commands.txt
+cat insert_test_data_hbase.txt | /home/hduser/hbase shell
 
-
-## Does the version of ycsb im installing cover more than hbase 98 & 94 ??
 echo 'Loading Hbase data before running workload ...'
-bin/ycsb load hbase -P workloads/workloada -cp /$HOME_NEW/hbase/conf -p table=usertable -p columnfamily=family
-
-
-echo 'Execute ycsb workload a (heavy with 50/50% Mix of Reads/Writes) against Hbase ...'
 cd $HOME_NEW
 cd ycsb-0.11.0
-bin/ycsb run hbase -P workloads/workloada -cp /$HOME_NEW/hbase/conf -p table=usertable -p columnfamily=family
+bin/ycsb load hbase098 -P workloads/workloada -cp /$HOME_NEW/hbase/conf -p table=usertable -p columnfamily=family
+
+echo 'Execute ycsb workload a (heavy with 50/50% Mix of Reads/Writes) against Hbase ...'
+bin/ycsb run hbase098 -P workloads/workloada -cp /$HOME_NEW/hbase/conf -p table=usertable -p columnfamily=family
