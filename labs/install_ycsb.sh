45845987cad4a224e67da388317a89e106f81504
RichardKavanagh <richard.kavanagh7@mail.dcu.ie>
2018-04-02 22:12:44 +0100

    Replaced curl with wget call

--- labs/install_ycsb.sh
+++ labs/install_ycsb.sh
@@ -15,14 +15,14 @@ else
 	useradd -p $(openssl passwd -1 $PASS) -m $USER
 	adduser hduser sudo
 fi
-	
+
 echo 'Logging in as hduser ...'
 HOME_NEW=/home/hduser
 exec sudo -u hduser /bin/sh - << eof
 cd $HOME_NEW
 
 echo 'Downloading YCSB ...'
-curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.11.0/ycsb-0.11.0.tar.gz
+wget https://github.com/brianfrankcooper/YCSB/releases/download/0.11.0/ycsb-0.11.0.tar.gz
 tar xzf ycsb-0.11.0.tar.gz
 
 echo 'Finished YCSB installation ...'
